ec2b31043dbb6a543042020dccc1ca289366b025
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-10-27 16:56:57 +0900

    setup pages, foursquare api, unsplash api.

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,18 @@ import Banner from '../components/banner';
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 import Card from '../components/card';
-import coffeeStores from '..//data/coffee-store.json';
+import { fetchCoffeeStores } from '../lib/coffee-stores';
 
-export default function Home() {
+export const getStaticProps = async (context) => {
+  const coffeeStores = await fetchCoffeeStores();
+  return {
+    props: {
+      coffeeStores,
+    },
+  };
+};
+
+export default function Home({ coffeeStores }) {
   const handleOnBannerBtnClick = () => {
     console.log('Hi banner button');
   };
@@ -30,19 +39,28 @@ export default function Home() {
             alt="a girl drinking coffee"
           />
         </div>
-        <div className={styles.cardLayout}>
-          {coffeeStores.map((coffeeStore) => {
-            return (
-              <Card
-                key={coffeeStore.id}
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl}
-                href={coffeeStore.href}
-                className={styles.card}
-              />
-            );
-          })}
-        </div>
+
+        {coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto stores</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      'https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80'
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
